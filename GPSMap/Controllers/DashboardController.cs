75f5c06b708d7edad981cd9da8eae9a4e7df71d0
mihiroza <mihir@deekshatech.in>
2020-06-03 08:44:45 -0700

    ttr report related changes

--- GPSMap/Controllers/DashboardController.cs
+++ GPSMap/Controllers/DashboardController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Diagnostics;
+using System.Configuration;
+using GPSMap.Models;
 
 namespace GPSMap.Controllers
 {
@@ -13,6 +16,52 @@ namespace GPSMap.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        //public ActionResult OpenExe()
+        //{
+        //    return File("D:\\weekly_reports\\tmp.txt", "text/plain");
+        //}
+
+        [HttpPost]
+        //public ActionResult OpenExe(SearchViewModel form, FormCollection formData)
+        public FileResult OpenExe(SearchViewModel form, FormCollection formData)
+        {
+            //openexe();
+            string exePath = ConfigurationManager.AppSettings["ExePath"];
+            string jsonConfigPath = ConfigurationManager.AppSettings["JsonConfigPath"];
+
+            try
+            {
+                //create json file
+                //change values in file
+
+                Process myProcess = new Process();
+                myProcess.StartInfo.UseShellExecute = false;
+                myProcess.StartInfo.FileName = exePath;
+                //myProcess.StartInfo.Arguments = "\"" + jsonConfigPath + "\"";
+                myProcess.StartInfo.Arguments = "-c " + jsonConfigPath;
+                myProcess.StartInfo.CreateNoWindow = true;
+                myProcess.Start();
+
+                //delete file
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            //return View();
+            //return null;
+
+            //return new EmptyResult();
+            //return File(Encoding.UTF8.GetBytes(sb.ToString()), "text/csv", "Ericsson.csv");
+            //return File(@"D:\weekly_reports\$2020_$06_$03_Cluster daily.ttr", "text/plain");
+            //return File(@"D:\weekly_reports\2020_05_26_Cluster daily.ttr", "text/plain", "ClusterReport.ttr");
+            return File(@"D:\weekly_reports\2020_05_26_Cluster daily.ttr", "text/plain", "ClusterReport.ttr");
+            //return File("D:\\weekly_reports\\tmp.txt", "text/plain");
+
+            //return new HttpStatusCodeResult(204);
+
         }
     }
 }
